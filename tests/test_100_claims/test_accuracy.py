a8b4b2792c5e6cd47def203448f6dddb0343832d
Jom Karlo Verzosa <jomkarlov@gmail.com>
2026-02-25 22:39:17 +0800

    fix: remove broken import

--- tests/test_100_claims/test_accuracy.py
+++ tests/test_100_claims/test_accuracy.py
@@ -3,7 +3,6 @@ import os
 import sys
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../../")))
-from constants.weights import score_to_label
 from controllers.v1.verify_controller import VerifyController
 
 DATASET_PATH = os.path.join(os.path.dirname(__file__), "test_dataset_generated.json")
