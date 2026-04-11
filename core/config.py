6550302a0af35ed445fe8088e2a696f373b11e9b
Jom Karlo Verzosa <jomkarlov@gmail.com>
2026-04-11 23:31:14 +0800

    Revert "feat/added local sqlite"
    This reverts commit ea0315e9e4342e6970efb3645303ee0b2a571c8f.

--- core/config.py
+++ core/config.py
@@ -11,6 +11,7 @@ DB_HOST = os.getenv("DB_HOST", "localhost")
 DB_PORT = os.getenv("DB_PORT", "5432")
 DATABASE_URI = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
+
 API_NAME = os.getenv("API_NAME")
 API_VERSION = os.getenv("API_VERSION", "v1")
 
